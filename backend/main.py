be67e90bf98d77f9bf19e055a96d90873f390713
Kelvin Lazzaris <98505501+KelvinLazzaris@users.noreply.github.com>
2025-06-23 00:30:04 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,11 +21,11 @@ origins = [
 
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 
